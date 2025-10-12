3e20f020045defdcfbc224c98b5a04d0f95ef235
Uros Zecevic <>
2025-10-12 16:14:25 +0200

    unit4_scale-excercise

--- unit4/unit4-scale_exercise.js
+++ unit4/unit4-scale_exercise.js
@@ -78,6 +78,10 @@ function fillScene() {
 	var sphere = new THREE.Mesh(
 		new THREE.SphereGeometry( 10, 32, 16 ), hourHandMaterial );
 	sphere.position.y = 18;	// move the hand above the other hand
+	sphere.scale.x = 3;
+	sphere.scale.y = 0.2;
+	sphere.scale.z = 0.2;
+	sphere.rotation.y = 30 * Math.PI / 180
 
 	// YOUR CODE HERE:
 
